3876a5f1b1cc31db35092f4bc4db336ba7d6a5bd
Atish Jain <atish_jain@yahoo.com>
2024-07-25 20:23:47 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,16 @@
+/*
+   Program name: Sample.java
+   Objective: Displays Welcome Message
+   Date: 22-July-2024
+   Author: Atish Jain
+   Company: CCA
+*/
+
+class Sample
+{
+ public static void main(String args[])
+  {
+    System.out.print("Welcome to Java Programming-");
+    System.out.println("Hyderabad");
+  }
+}
---
+++ VariableDemo.java
@@ -0,0 +1,36 @@
+
+class VariableDemo
+{
+	public static void main(String args[])
+	{
+		byte id=101;
+		short rollno=1001;
+		int accno=987890;
+		String name="Sravan";
+		long mno=9949128444L;
+		float height=5.6F;
+		double salary=89000.00;
+		char gen='M';
+		
+		System.out.println("ID:"+id);
+		System.out.println("Rollno:"+rollno);
+		System.out.println("Account no:"+accno);
+		System.out.println("Name:"+name);
+		System.out.println("Mobile no:"+mno);
+		System.out.println("Height:"+height);
+		System.out.println("Salary:"+salary);
+		System.out.println("Gender:"+gen);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
