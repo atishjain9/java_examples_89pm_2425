68b7469c319ac020612f45405f53b03c1b35afd9
Atish Jain <atish_jain@yahoo.com>
2024-07-31 21:35:38 +0530

    Add files via upload

---
+++ P013_Binary.java
@@ -0,0 +1,14 @@
+class P013_Binary 
+{
+	public static void main(String[] args) 
+	{
+		int no=25;
+		System.out.println("Binary of 25 is:"+Integer.toBinaryString(no));
+		System.out.println("Binary of 10 is:"+Integer.toBinaryString(10));
+		System.out.println("Binary of 5 is:"+Integer.toBinaryString(5));
+		System.out.println("Binary of -5 is:"+Integer.toBinaryString(-5));
+
+
+		
+	}
+}
--- Sample.java
+++ Sample.java
@@ -1,16 +1,8 @@
-/*
-   Program name: Sample.java
-   Objective: Displays Welcome Message
-   Date: 22-July-2024
-   Author: Atish Jain
-   Company: CCA
-*/
-
-class Sample
+public class Sample 
 {
- public static void main(String args[])
-  {
-    System.out.print("Welcome to Java Programming-");
-    System.out.println("Hyderabad");
-  }
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("File name and class names are different");
+	}
 }
